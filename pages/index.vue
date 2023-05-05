2c15a7602d30c208608fa63a47aa62c1bd4a5674
河村 泰佑 <kata.kawa813@gmail.com>
2023-05-06 08:44:10 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="p-8">
+    <h1 class="dark:bg-black dark:text-white text-3xl font-bold underline">
+      Index.vue
+    </h1>
+    <p class="dark:bg-black dark:text-white">{{ $colorMode.preference }}</p>
+    <div class="space-x-2">
+      <button
+        class="px-2 border border-1"
+        @click="$colorMode.preference = 'dark'"
+      >
+        dark
+      </button>
+      <button
+        class="px-2 border border-1"
+        @click="$colorMode.preference = 'light'"
+      >
+        light
+      </button>
+    </div>
+  </div>
+</template>
